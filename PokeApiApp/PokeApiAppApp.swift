ccf808d3e89453312e08925b8b7675a424db1be9
ju4nko <ju4nko@gmail.com>
2026-07-16 08:35:04 +0200

    Initial Commit

---
+++ PokeApiApp/PokeApiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeApiAppApp.swift
+//  PokeApiApp
+//
+//  Created by Juanjo on 16/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct PokeApiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
